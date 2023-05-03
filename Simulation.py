6fb48578ba20281d8e694af7addd1121b34df869
Tankinator175 <89499029+Tankinator175@users.noreply.github.com>
2023-05-03 14:01:41 -0600

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -1,1 +1,19 @@
-#start here
+# import random library
+
+# get popluation counts from user
+
+# start loop
+
+  # find ratio
+  
+  # use ratio to find difficulty (Joseph)
+  
+  # generate hunt success modifier (Joseph)
+  
+  # find number of killed prey (Joseph)
+  
+  # resolve prey births and deaths
+  
+  # resolve predator births and deaths (Joseph)
+  
+  # print outputs

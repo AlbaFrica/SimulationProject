586c67044656d5c91be590c48886d9647a4263d6
Tankinator175 <89499029+Tankinator175@users.noreply.github.com>
2023-05-05 11:16:19 -0600

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -1,9 +1,12 @@
 # import random library - Jenaka
 import random
 
+# initialize year count
+year = 0
+
 # get popluation counts from user - Jenaka
-prey = 
-predator = 
+prey = input("How many prey are in the ecosystem? \(RECOMMENDED TO BE IN THE UPPER 100 000s\)")
+predator = input("How many predators are in the ecosystem? \(RECOMMENDED TO BE IN THE LOWER 1000s\)")
 # start loop - Jenaka
 
   # find ratio (Melody)
@@ -19,5 +22,7 @@ predator =
   # resolve predator births and deaths (Joseph)
   
   # print outputs (Melody)
+  
+  # advance time 
   
   # ask user if program should continue for another 10 years

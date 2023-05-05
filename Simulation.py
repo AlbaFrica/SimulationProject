f9c7748b3440b0145c43765f0fd11718f137420b
AlbaFrica <123906967+AlbaFrica@users.noreply.github.com>
2023-05-05 11:56:21 -0600

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -6,18 +6,18 @@ year = 0
 shouldContinue = True
 
 # get popluation counts from user
-prey = input("How many prey are in the ecosystem? \(RECOMMENDED TO BE IN THE UPPER 100 000s\) ")
-predator = input("How many predators are in the ecosystem? \(RECOMMENDED TO BE IN THE LOWER 1000s\) ")
+prey = int(input("How many prey are in the ecosystem? \(RECOMMENDED TO BE IN THE UPPER 100 000s\) "))
+predator = int(input("How many predators are in the ecosystem? \(RECOMMENDED TO BE IN THE LOWER 1000s\) "))
 
 # start loop
-while shouldContinue == True
+while shouldContinue == True:
 
   # advance time
-  global year
   year +=1
 
   # find ratio (Melody)
   ratio = prey / predator
+  print("The starting ratio is " + str(ratio))
   
   # use ratio to find difficulty
   difficulty = ((ratio - 350)/20)+1
@@ -26,23 +26,26 @@ while shouldContinue == True
   successModifier = random.randint(85, 115)/100
   
   # find number of killed prey
-  hunted = (predators * successModifier) * (17.5 * difficulty)
+  hunted = (predator * successModifier) * (17.5 * difficulty)
   
   # resolve prey births and deaths
-  global prey
-  prey *= random.randint(10, 25)/100+1
+  prey *= random.randint(5, 8)/100+1
   prey -= - hunted
+  if hunted > prey:
+    prey = 0
   
   # resolve predator births and deaths
-  global predator
   predator *= random.randint(20, 35)/100+1
-  predator -= ((predators * 15) - hunted)/5
+  predator -= ((predator * 15) - hunted)/5
+  predator *= .85
+  if predator < 0:
+    predator = 0
   
   # print outputs
   print("YEAR" + str(year))
-  print("There are currently " + str(prey) + " left.")
-  print("There are currently " + str(predator) + " left.")
-  print(ratio)
+  print("There are currently " + str(prey) + " prey left.")
+  print("There are currently " + str(predator) + " predators left.")
+  print("Prey/Predator ratio = " + str(ratio))
   
   # ask user if program should continue for another 10 years
   if year % 10 == 0:

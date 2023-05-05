c961f31dc15ef29bffa5621be0c4ccb9bc16b86f
Tankinator175 <89499029+Tankinator175@users.noreply.github.com>
2023-05-05 11:42:37 -0600

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -1,35 +1,49 @@
-# import random library - Jenaka
+# import random library 
 import random
 
 # setup passage of time and loop
 year = 0
 shouldContinue = True
-# get popluation counts from user - Jenaka
+
+# get popluation counts from user
 prey = input("How many prey are in the ecosystem? \(RECOMMENDED TO BE IN THE UPPER 100 000s\) ")
 predator = input("How many predators are in the ecosystem? \(RECOMMENDED TO BE IN THE LOWER 1000s\) ")
-# start loop - Jenaka
+
+# start loop
 while shouldContinue == True
-  # find ratio (Melody)
-  ratio = prey / predator
-  # use ratio to find difficulty (Joseph)
-  difficulty = ((ratio - 350)/20)+1
-  # generate hunt success modifier (Joseph)
-  successModifier = random.randint(85, 115)/100
-  # find number of killed prey (Joseph)
-  hunted = (predators * successModifier) * (17.5 * difficulty)
-  # resolve prey births and deaths(Melody)
-  
-  # resolve predator births and deaths (Joseph)
-  global predator
-  predator += predator * random.randint(20, 35)/100+1
-  predator -= 
-  # print outputs (Melody)
-  print("There are currently " + str(prey) + " left.")
-  print("There are currently " + str(predator) + "left.")
-  print(ratio)
-    # advance time
+
+  # advance time
   global year
   year +=1
+
+  # find ratio (Melody)
+  ratio = prey / predator
+  
+  # use ratio to find difficulty
+  difficulty = ((ratio - 350)/20)+1
+  
+  # generate hunt success modifier
+  successModifier = random.randint(85, 115)/100
+  
+  # find number of killed prey
+  hunted = (predators * successModifier) * (17.5 * difficulty)
+  
+  # resolve prey births and deaths
+  global prey
+  prey *= random.randint(10, 25)/100+1
+  prey -= - hunted
+  
+  # resolve predator births and deaths
+  global predator
+  predator *= random.randint(20, 35)/100+1
+  predator -= ((predators * 15) - hunted)/5
+  
+  # print outputs
+  print("YEAR" + str(year))
+  print("There are currently " + str(prey) + " left.")
+  print("There are currently " + str(predator) + " left.")
+  print(ratio)
+  
   # ask user if program should continue for another 10 years
   if year % 10 == 0:
     ask = input("Would you like to continue the simulation for another 10 years? (y/n) ")
